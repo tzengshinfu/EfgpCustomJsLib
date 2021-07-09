e464d4a0a9b730bf30ea32c455b0749cb6f15e66
tzengshinfu <tzengshinfu@gmail.com>
2021-07-09 20:18:15 +0800

    修正:SystemVariables.js變數注釋格式

--- Source/SystemVariables.js
+++ Source/SystemVariables.js
@@ -1,17 +1,85 @@
-/** 使用者代號 */ /** @type {string} */ var userId;
-/** 使用者姓名 */ /** @type {string} */ var userName;
-/** 主部門職稱 */ /** @type {string} */ var mainFunctionName;
-/** 主部門組織代號 */ /** @type {string} */ var mainOrgId;
-/** 主部門組織名稱 */ /** @type {string} */ var mainOrgName;
-/** 主部門代號 */ /** @type {string} */ var mainOrgUnitIds;
-/** 主部門名稱 */ /** @type {string} */ var mainOrgUnitNames;
-/** 活動代號 */ /** @type {string} */ var activityId;
-/** 活動名稱 */ /** @type {string} */ var activityName;
-/** 預設代理人代號 */ /** @type {string} */ var defaultSubstituteId;
-/** 預設代理人姓名 */ /** @type {string} */ var defaultSubstituteName;
-/** 表單代號 */ /** @type {string} */ var formId;
-/** 表單名稱 */ /** @type {string} */ var formName;
-/** 流程代號 */ /** @type {string} */ var processId;
-/** 流程名稱 */ /** @type {string} */ var processName;
-/** 流程序號 */ /** @type {string} */ var serialNumber;
-/** 系統日期 */ /** @type {string} */ var systemDateTime;
+/**
+ * 使用者代號
+ * @type {string}
+ */
+var userId;
+/**
+ * 使用者姓名
+ * @type {string}
+ */
+var userName;
+/**
+ * 主部門職稱
+ * @type {string}
+ */
+var mainFunctionName;
+/**
+ * 主部門組織代號
+ * @type {string}
+ */
+var mainOrgId;
+/**
+ * 主部門組織名稱
+ * @type {string}
+ */
+var mainOrgName;
+/**
+ * 主部門代號
+ * @type {string}
+ */
+var mainOrgUnitIds;
+/**
+ * 主部門名稱
+ * @type {string}
+ */
+var mainOrgUnitNames;
+/**活動代號
+ * @type {string}
+ */
+var activityId;
+/**
+ * 活動名稱
+ * @type {string}
+ */
+var activityName;
+/**
+ * 預設代理人代號
+ * @type {string}
+ */
+var defaultSubstituteId;
+/**
+ * 預設代理人姓名
+ * @type {string}
+ */
+var defaultSubstituteName;
+/**
+ * 表單代號
+ * @type {string}
+ */
+var formId;
+/**
+ * 表單名稱
+ * @type {string}
+ */
+var formName;
+/**
+ * 流程代號
+ * @type {string}
+ */
+var processId;
+/**
+ * 流程名稱
+ * @type {string}
+ */
+var processName;
+/**
+ * 流程序號
+ * @type {string}
+ */
+var serialNumber;
+/**
+ * 系統日期
+ * 格式(yyyy/MM/dd)
+ * @type {string}
+ */
+var systemDateTime;
